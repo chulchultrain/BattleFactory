7138458f42d434f86e6ec69e4eb6fbfdbe4cf52a
Thomas Yang <yangchulmin0@gmail.com>
2015-04-10 19:00:47 -0500

    Added PokemonStats for Attack, Defense, SpecialAttack, SpecialDefense, Speed

---
+++ PokemonStats.h
@@ -0,0 +1,19 @@
+#ifndef __POKEMONSTATS_H_INCLUDED__
+#define __POKEMONSTATS_H_INCLUDED__
+
+
+typedef struct PokemonStats{
+
+int HitPoints;
+int Attack;
+int Defense;
+int SpecialAttack;
+int SpecialDefense;
+int Speed;
+
+//enum for pokemon Nature
+
+} PokemonStats;
+
+
+#endif
