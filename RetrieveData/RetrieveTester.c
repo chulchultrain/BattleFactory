75eaa94fb35834649a864e2f0ea70367711d2387
Thomas Yang <yangchulmin0@gmail.com>
2015-09-03 00:55:50 -0500

    Updated makefile so that other people can have different directory structure set up and still use.

--- RetrieveData/RetrieveTester.c
+++ RetrieveData/RetrieveTester.c
@@ -1,11 +1,22 @@
 #include <RetrieveData/RetrievePokemon.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
-char name[21] = "Charizard";
-ConsolePrintPokemonEntryFile(name);
+//TODO:change with makefile to nonmagic value
+char buffer[21] = {0};
 
+printf("The Application to Help Get Higher Records in Pokemon Platinum Battle Factory\n");
+
+unsigned int toContinue = 1;
+
+while(toContinue == 1) {
+
+fgets(buffer,21,stdin);
+ConsolePrintPokemonEntryFile(buffer);
+fgetc(buffer,21,stdin);
+}
 return 0;
 
 }
