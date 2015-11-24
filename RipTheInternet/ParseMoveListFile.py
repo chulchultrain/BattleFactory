382b65da757a0f7ba75f25febe16c66956afe4f5
Thomas Yang <yangchulmin0@gmail.com>
2015-11-24 01:25:39 -0600

    Added name parse to ParseMoveListFile.py

--- RipTheInternet/ParseMoveListFile.py
+++ RipTheInternet/ParseMoveListFile.py
@@ -9,6 +9,10 @@
 
 processFinished = False
 
+tagBegin = '</wiki/'
+
+moveTagEnd = '_(move)>'
+
 unrefinedDir = "../BASE/UNREFINED/"
 
 inputDir = unrefinedDir + "/UNFILTERED_MOVE_LIST/"
@@ -19,6 +23,10 @@ outputFileName = "Filtered_Move_List.txt"
 
 startingLine="</wiki/Power> 	Accuracy </wiki/Accuracy> 	Gen </wiki/Generation>\n"
 
+def TagTokenStart(ch):
+	if ch == '<':
+		return True
+	return False
 
 def TrimBeginningWhiteSpace(line):
 	i = 0
@@ -33,6 +41,25 @@ def TrimBeginningWhiteSpace(line):
 	else:
 		return line
 
+def TrimEndingWhiteSpace(line):
+	i = len(line) - 1
+	while i >= 0 and line[i].isspace():
+		i -= 1
+	if i < len(line) - 1:
+		return line[:i+1]
+	else:
+		return line
+
+def ExistsMoveTagInLine(line):
+	i = 0
+	#as of right now, it may accept all tags, when necessary change to just move tags
+	while i < len(line) and line[i] != '<':
+		i += 1
+	if i < len(line):
+		return True
+	else:
+		return False
+
 def IsNumber(ch):
 	if ch >= '0' and ch <= '9':
 		return True
@@ -43,23 +70,50 @@ def ParseIndexFromLine(line):
 	i = 0
 	index = ''
 	while i < len(line) and IsNumber(line[i]):
-		i += 1
 		index += line[i]
+		i += 1
 
 	if i >= len(line):
-		return None,None
+		return index,''
 	else:
 		line = line[i:]
 		line = TrimBeginningWhiteSpace(line)
 		return index,line
 
+def ParseNameFromLine(line):
+	i = 0
+	name = ''
+	i = line.find(tagBegin) + len(tagBegin)
+	if i >= len(line):
+		return None,''
+	else:
+		line = line[i:]
+		i = 0
+
+	i = line.find(moveTagEnd)
+
+	if i >= len(line):
+		return None,''
+	else:
+		name = line[0:i]
+		i += len(moveTagEnd)
+		if i >= len(line):
+			return name,''
+		else:
+			return name,line[i:]
+	
+
 def ParseMove(fin):
 
 	line = fin.readline()	
 
+	index = name = moveType = moveCategory = damage = ''
+
 	#Parse Index
 	index, line = ParseIndexFromLine(line)
 	#Parse Name
+	if ExistsMoveTagInLine(line):
+		name,line = ParseNameFromLine(line)
 
 	#Parse Move Type
 
@@ -76,7 +130,8 @@ def ParseMove(fin):
 #def ParseMoveFile(inputFileFullName,outputFileFullName):
 #	global processFinished
 	
-gg = "123 Chimchar\n"
-print ParseIndexFromLine(gg) 
-
-
+gg = "123 Chimchar </wiki/Chimchar_(move)>\n"
+index,gg = ParseIndexFromLine(gg) 
+print index,gg
+name,gg = ParseNameFromLine(gg)
+print name,gg
